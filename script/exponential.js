102b60ca4d2c3297a317f11b4cd7ca42f7d68142
unknown <jefferson.hovey@gmail.com>
2023-06-08 06:14:50 -0400

    bounded rightIndex in exponential.js

--- script/exponential.js
+++ script/exponential.js
@@ -32,35 +32,25 @@ async function exponentialSearch(target) {
   await sleep(4000);
 
   // Binary search:
-  let leftIndex = (i >> 1) + 1;
-  let rightIndex = i - 1;
-
+  let leftIndex = (i * .5) + 1;
+  let rightIndex = Math.min(i - 1, arr.length - 1);
+  
   while (rightIndex >= leftIndex) {
-    let pivot = leftIndex + Math.floor((rightIndex - leftIndex) / 2); 
-    
-    if (pivot >= arr.length) {
-      oobColor(pivot);
-      await sleep(4000);
-    } else {
-      colorPivot(pivot);
+    let pivot = leftIndex + Math.floor((rightIndex - leftIndex) / 2);
+    colorPivot(pivot);
     await sleep(2000);
-    }
-    
-    if (arr[pivot] === undefined) {
-      rightIndex = pivot - 1;
-    } else if (target < arr[pivot]) {
+
+    if (target < arr[pivot]) {
       color("white", pivot, arr.length - 1);
       await sleep(2000);
       rightIndex = pivot - 1;
     } else if (target === arr[pivot]) {
       found(target, pivot);
       return;
-    } else if (target > arr[pivot]) {
+    } else {
       color("white", 0, pivot);
       await sleep(2000);
       leftIndex = pivot + 1;
-    } else {
-      throw new Error("Error in oneSidedSearch function");
     }
   }
 
